e1b110af71cdee856a9250a3b578efc7a1f53b85
Vigneh-sd <svigneh8@gmail.com>
2025-04-05 05:11:46 +0530

    WEEK 1 TO WEEK 4 ARRAYS BASIC

---
+++ Arrays_Nums_basic/sortAnArrayZeroOneTwo.java
@@ -0,0 +1,39 @@
+
+import java.util.Arrays;
+
+public class sortAnArrayZeroOneTwo{
+
+    public static int []solution(int arr[], int n) {
+      int low=0,mid=0,high=arr.length-1;
+     
+        while(mid<=high){
+        if(arr[mid]==0)
+        {
+          int temp = arr[mid];
+          arr[mid]=arr[low];
+          arr[low]=temp;
+          low++;
+          mid++;
+        }
+        else if(arr[mid]==1)
+        {
+          mid++;
+        }
+        else{
+           int temp = arr[mid];
+           arr[mid]=arr[high];
+          arr[high]=temp;
+          high--;
+        }
+        }
+       
+
+    return arr;
+    }
+    public static void main(String[] args) {
+        int arr[] = new int[]{0,2,1,2,0};
+        int n=5;
+        int ans[] = solution(arr, n);
+        System.out.println(Arrays.toString(ans));
+    }
+}
